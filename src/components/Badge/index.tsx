8b246ccbfb557aac8880f766be39abddb74f66c2
Crummorn <igorcaze@gmail.com>
2022-04-02 11:39:53 -0300

    Chore: Create Badge component.

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { TypeIcon } from 'components';
+
+import * as S from './styles';
+
+export interface BadgeProps {
+  type?:
+    | 'bug'
+    | 'dark'
+    | 'dragon'
+    | 'electric'
+    | 'fairy'
+    | 'fighting'
+    | 'fire'
+    | 'flying'
+    | 'ghost'
+    | 'grass'
+    | 'ground'
+    | 'ice'
+    | 'normal'
+    | 'poison'
+    | 'psychic'
+    | 'rock'
+    | 'steel'
+    | 'water';
+}
+
+const Badge: React.FC<BadgeProps> = ({ type = 'normal' }: BadgeProps) => (
+  <S.Container type={type}>
+    <TypeIcon type={type} />
+
+    {type}
+  </S.Container>
+);
+
+export default Badge;
---
+++ src/components/Badge/styles.ts
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components';
+
+import { BadgeProps } from './index';
+
+export const Container = styled.span<BadgeProps>`
+  ${({ theme, type }) => css`
+    display: inline-flex;
+    padding: 0.5rem;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    background: ${theme.colors.type.color[type || 'normal']};
+    border-radius: 0.3rem;
+
+    color: ${theme.colors.text.white};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.weight.medium};
+    text-transform: capitalize;
+
+    svg {
+      width: 1.5rem;
+      height: 1.5rem;
+
+      path {
+        fill: ${theme.colors.text.white};
+      }
+    }
+  `}
+`;
