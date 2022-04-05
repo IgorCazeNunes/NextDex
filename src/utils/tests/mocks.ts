7aefcbbc2cb44982e8e67c210929718fc7399d9d
Crummorn <igorcaze@gmail.com>
2022-04-05 16:16:13 -0300

    Test: Create helper with mocked values.

---
+++ src/utils/tests/mocks.ts
@@ -0,0 +1,42 @@
+import { IPokemonCardProps } from 'components/PokemonCard';
+
+const pokemonList: IPokemonCardProps[] = [
+  {
+    id: '001',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+  {
+    id: '002',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+  {
+    id: '003',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+  {
+    id: '004',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+  {
+    id: '005',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+  {
+    id: '006',
+    name: 'charizard',
+    types: ['fire', 'flying'],
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  },
+];
+
+export { pokemonList };
