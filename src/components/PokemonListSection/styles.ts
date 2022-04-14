996117976602c61fa92fd4ba1068538ac99f121a
Crummorn <igorcaze@gmail.com>
2022-04-14 10:39:20 -0300

    Chore: Adjust styles to adapt with ResponsiveContainer.

--- src/components/PokemonListSection/styles.ts
+++ src/components/PokemonListSection/styles.ts
@@ -1,7 +1,15 @@
+import { ResponsiveContainer } from 'components/ResponsiveContainer/styles';
+
 import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${ResponsiveContainer} {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
