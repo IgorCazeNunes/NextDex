41c656562860af046c32fae63a14b7133f602612
Crummorn <igorcaze@gmail.com>
2022-06-18 16:35:12 -0300

    feat: integrate list with api.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,79 @@
-import HomeTemplate, { IHomeTemplateProps } from '../templates/Home';
+import { TPokemonType } from 'components/molecules/TypeBadge';
+import { IPokemonCardProps } from 'components/organisms/PokemonCard';
+import { useInfiniteQuery } from 'react-query';
+import HomeTemplate from '../templates/Home';
 
-export default function Home(props: IHomeTemplateProps) {
-  return <HomeTemplate {...props} />;
+interface IPokemonData {
+  id: string;
+  name: string;
+  sprites: {
+    other: {
+      'official-artwork': {
+        // eslint-disable-next-line camelcase
+        front_default: string;
+      };
+    };
+  };
+  types: { slot: number; type: { name: TPokemonType } }[];
 }
 
-export function getServerSideProps() {
-  return {
-    props: {
-      list: [
-        {
-          id: '001',
-          name: 'bulbasaur',
-          types: ['grass', 'poison'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-        },
-        {
-          id: '002',
-          name: 'ivysaur',
-          types: ['grass', 'poison'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
-        },
-        {
-          id: '003',
-          name: 'venusaur',
-          types: ['grass', 'poison'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
-        },
-        {
-          id: '004',
-          name: 'charmander',
-          types: ['fire'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
-        },
-        {
-          id: '005',
-          name: 'charmeleon',
-          types: ['fire'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
-        },
-        {
-          id: '006',
-          name: 'charizard',
-          types: ['fire', 'flying'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
-        },
-        {
-          id: '007',
-          name: 'squirtle',
-          types: ['water'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
-        },
-        {
-          id: '008',
-          name: 'wartortle',
-          types: ['water'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
-        },
-        {
-          id: '009',
-          name: 'blastoise',
-          types: ['water'],
-          image:
-            'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png',
-        },
-      ],
-    },
+export default function Home() {
+  const fetchPokemon = async ({ pageUrl = '' }) => {
+    const request = await fetch(pageUrl);
+    const pokemon = await request.json();
+    return pokemon;
   };
+
+  const fetchPokemonList = async ({
+    pageParam = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=18',
+  }) => {
+    const { results, next } = await fetch(pageParam).then((response) =>
+      response.json(),
+    );
+
+    const pokemonList = await Promise.all(
+      results.map(({ url = '' }) => url && fetchPokemon({ pageUrl: url })),
+    );
+
+    const formattedPokemonList: IPokemonCardProps[] = pokemonList.map(
+      (pokemon: IPokemonData) => {
+        const typeList = pokemon.types.map((type) => type.type.name);
+
+        return {
+          id: pokemon.id,
+          name: pokemon.name,
+          types: typeList,
+          image: pokemon.sprites.other['official-artwork'].front_default,
+        };
+      },
+    );
+
+    return { response: formattedPokemonList, nextPage: next };
+  };
+
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery('pokemons', fetchPokemonList, {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    });
+
+  if (isLoading || !data) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <HomeTemplate list={data?.pages.map((page) => page.response).flat()} />
+
+      {hasNextPage && (
+        <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          Load more pokemons...
+        </button>
+      )}
+
+      {isFetchingNextPage && <p>Loading...</p>}
+    </>
+  );
 }
