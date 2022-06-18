3ea7849d262ed796da5425977c5b8f114a6aad6c
Crummorn <igorcaze@gmail.com>
2022-06-18 16:32:39 -0300

    Chore: config react-query context.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,34 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>ReactDex</title>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>ReactDex</title>
 
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
 
-        <meta name="theme-color" content="#ea5d60" />
-      </Head>
+            <meta name="theme-color" content="#ea5d60" />
+          </Head>
 
-      <GlobalStyles />
+          <GlobalStyles />
 
-      <Component {...pageProps} />
-    </ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
